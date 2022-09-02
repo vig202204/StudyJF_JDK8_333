4e7aa666ae2d41cba8f7e7f2bb14256786b7ac4a
ihorvoloshyn <vig092007@gmail.com>
2022-09-02 11:42:12 +0300

    Перенесення апплікації CalculatorJF.java до середи розробки із JDK 8.333

---
+++ src/main/java/capter_6_Calculator/CalculatorEngineJF.java
@@ -0,0 +1,127 @@
+package capter_6_Calculator;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+
+public class CalculatorEngineJF implements ActionListener{
+
+    CalculatorJF parent; //посилання на вікно калькулятора
+
+    char selectedAction = ' '; // +, -, /, або *
+    double currentResult = 0;
+
+    //для контроля подвійної крапки
+    int countDoublePoint =0;
+
+    //Конструктор зберігає посилання на вікно калькулятора
+// у змінній екземпляра класу
+    CalculatorEngineJF (CalculatorJF parent) {
+        this.parent = parent;
+    }
+
+    public void actionPerformed (ActionEvent e){
+
+        //Отримати джерело дії
+        JButton clickedButton = (JButton) e.getSource();
+        String dispFieldText = parent.displayField.getText();
+        double displayValue = 0;
+
+//Отримати число з дисплея калькулятора,
+// якщо він не порожній.
+//Знак оклику - це оператор заперечення
+        if (!"".equals(dispFieldText)) {
+                displayValue = Double.parseDouble(dispFieldText);
+        }
+        Object src = e.getSource();
+
+//Для кожної кнопки арифметичної дії
+//запам'ятати її:+,-,/, або *, зберегти поточне число
+//у змінній currentResult, і очистити дисплей
+//для введення нового числа
+        if (src == parent.buttonPlus) {
+            selectedAction = '+';
+            currentResult = displayValue;
+            parent.displayField.setText ("");
+        } else if (src == parent.buttonMinus) {
+            selectedAction = '-';
+            currentResult = displayValue;
+            parent.displayField.setText ("");
+        } else if (src == parent.buttonDivide) {
+            selectedAction = '/';
+            currentResult = displayValue;
+            /*
+            Практичні вправи:
+            Спробуйте розділити число на нуль за допомогою на-
+            шого калькулятора - дисплей покаже слово Infinity.
+            Змініть клас CalculatorEngine, щоб відображалося по-
+            відомлення "На нуль ділити не можна", якщо кори-
+            стувач натисне на кнопку "Розділити", коли дисплей калькулятора буде порожній.
+             */
+            if (displayValue == 0.0){
+                parent.displayField.setText ("На нуль ділити не можна");
+            } else
+                parent.displayField.setText ("");
+        } else if (src == parent.buttonMultiply) {
+            selectedAction = '*';
+            currentResult = displayValue;
+            parent.displayField.setText ("");
+        } else if (src == parent.buttonEqual) {
+            // listFromDisplay.clear();             // vig
+            //Здійснити арифметичну дію, залежно
+            //від selectedAction, оновити змінну currentResult
+            //і показати результат на дисплеї
+            if (selectedAction == '+') {
+                currentResult += displayValue;
+                //Конвертувати результат в рядок, додаючи його
+                // до порожнього рядку і показати його
+                parent.displayField.setText(""+currentResult);
+            } else if (selectedAction == '-') {
+                currentResult -= displayValue;
+                parent.displayField.setText(""+currentResult);
+            } else if (selectedAction == '/') {
+                currentResult /= displayValue;
+                parent.displayField.setText(""+currentResult);
+            } else if (selectedAction == '*') {
+                currentResult *= displayValue;
+                parent.displayField.setText(""+currentResult);
+            }
+        } else {
+
+            //Для всіх цифрових кнопок приєднати напис на
+            //кнопці до напису в дисплеї
+            String clickedButtonLabel = clickedButton.getText();
+
+          /*  Практичні вправи для розумників і розумниць:
+             Змініть клас CalculatorEngine, щоб заборонити вводити
+             більше однієї крапки в числі.
+              Підказка: прочитайте про метод indexOf() класу String,
+              щоб дізнатися, чи є вже в числі крапка.*/
+
+            String predispFieldText = dispFieldText +
+                    clickedButtonLabel;
+            // Якщо крапки немає
+            if (predispFieldText.indexOf(".") == -1) {
+                parent.displayField.setText(dispFieldText + clickedButtonLabel);
+                String rezultdispFieldText = predispFieldText;
+            } else {
+                // Якщо є крапка
+                if (countDoublePoint == 0) {
+                    // и вон перша, то додаємо до строки калькулятора
+                    parent.displayField.setText(dispFieldText + clickedButtonLabel);
+                    countDoublePoint++;
+                } else if (countDoublePoint >= 1) {
+                    // а якщо крапка не перша, то відкидаємо її
+                    // і залишаємо строку вводу калькулятора без змін
+                    if (clickedButtonLabel == ".") {
+                        System.out.println("Exeption of Double Point = " + countDoublePoint);
+                        parent.displayField.setText(dispFieldText);
+                    } else {
+                        parent.displayField.setText(dispFieldText + clickedButtonLabel);
+                        System.out.println(dispFieldText + clickedButtonLabel);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/capter_6_Calculator/CalculatorJF.java
@@ -0,0 +1,112 @@
+package capter_6_Calculator;
+import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+
+public class CalculatorJF {
+
+    //Оголошуємо та ініціалізуємо компоненти вікна
+    JButton button0=new JButton("0");
+    JButton button1=new JButton("1");
+    JButton button2=new JButton("2");
+    JButton button3=new JButton("3");
+    JButton button4=new JButton("4");
+    JButton button5=new JButton("5");
+    JButton button6=new JButton("6");
+    JButton button7=new JButton("7");
+    JButton button8=new JButton("8");
+    JButton button9=new JButton("9");
+
+    JButton buttonPoint=new JButton(".");
+    JButton buttonEqual=new JButton("=");
+    JButton buttonPlus=new JButton("+");
+    JButton buttonMinus=new JButton("-");
+    JButton buttonDivide=new JButton("/");
+    JButton buttonMultiply=new JButton("*");
+
+    JPanel windowContent=new JPanel();
+    JTextField displayField=new JTextField(30);
+
+    // Конструктор
+    CalculatorJF(){
+
+        //Встановити менеджер розташування для панелі
+        BorderLayout bl = new BorderLayout();
+        windowContent.setLayout (bl);
+
+        //Додаємо дисплей у верхній частині вікна
+        windowContent.add("North", displayField);
+
+        //Створюємо панель з менеджером розташування
+        //GridLayout в якій буде 12 кнопок - 10 цифр, та
+        //кнопки "крапка" і "дорівнює"
+        JPanel p1 = new JPanel();
+        GridLayout gl = new GridLayout(4,3);
+        p1.setLayout(gl);
+        p1.add(button1);
+        p1.add(button2);
+        p1.add(button3);
+        p1.add(button4);
+        p1.add(button5);
+        p1.add(button6);
+        p1.add(button7);
+        p1.add(button8);
+        p1.add(button9);
+        p1.add(button0);
+        p1.add(buttonPoint);
+        p1.add(buttonEqual);
+
+        //Додаємо панель p1 в центр вікна
+        windowContent.add("Center",p1);
+
+        //Створюємо панель з менеджером розташування
+        //GridLayout на якій буде 4 кнопки -
+        //Плюс, Мінус, Розділити та Помножити
+        JPanel p2 = new JPanel();
+        GridLayout gl2 = new GridLayout(4,1);
+        p2.setLayout(gl2);
+        p2.add(buttonPlus);
+        p2.add(buttonMinus);
+        p2.add(buttonMultiply);
+        p2.add(buttonDivide);
+
+        //Додаємо панель p2 в праву частину вікна
+        windowContent.add("East",p2);
+
+        //Створюємо frame і додаємо в нього вміст
+        JFrame frame = new JFrame ("Calculator");
+        frame.setContentPane(windowContent);
+
+        //Встановлюємо розмір вікна, так щоб вмістилися
+        //Всі компоненти
+        frame.pack();
+
+        //Показуємо вікно
+        frame.setVisible (true);
+
+        //Створюємо екземпляр слухача подій і
+        //Реєструємо його в кожній кнопці
+        CalculatorEngineJF calcEngineJF = new CalculatorEngineJF(this);
+        button0.addActionListener(calcEngineJF);
+        button1.addActionListener(calcEngineJF);
+        button2.addActionListener(calcEngineJF);
+        button3.addActionListener(calcEngineJF);
+        button4.addActionListener(calcEngineJF);
+        button5.addActionListener(calcEngineJF);
+        button6.addActionListener(calcEngineJF);
+        button7.addActionListener(calcEngineJF);
+        button8.addActionListener(calcEngineJF);
+        button9.addActionListener(calcEngineJF);
+        buttonPoint.addActionListener(calcEngineJF);
+        buttonPlus.addActionListener(calcEngineJF);
+        buttonMinus.addActionListener(calcEngineJF);
+        buttonDivide.addActionListener(calcEngineJF);
+        buttonMultiply.addActionListener(calcEngineJF);
+        buttonEqual.addActionListener(calcEngineJF);
+    }
+    public static void main(String[] args) {
+
+        //Створюємо екземпляр класу "Калькулятор"
+        CalculatorJF calc = new CalculatorJF();
+    }
+}
