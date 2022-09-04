e5b0f481c43b7925a34aa148513ea914802a0f57
ihorvoloshyn <vig092007@gmail.com>
2022-09-04 17:16:58 +0300

    (Я.Ф. Розділ 7, стор. 126). робота з аплетами від java.applet.Applet

---
+++ src/main/java/chapter_7_TicTacToe/HelloApplet.java
@@ -0,0 +1,8 @@
+package chapter_7_TicTacToe;
+
+
+public class HelloApplet extends java.applet.Applet {
+    public void paint(java.awt.Graphics graphics) {
+        graphics.drawString("Привіт, Світ!", 70, 40);
+    }
+}
