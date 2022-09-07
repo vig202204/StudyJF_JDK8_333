796fa2f1b9ad38e261ee63e818db27f50e454116
ihorvoloshyn <vig092007@gmail.com>
2022-09-07 16:01:15 +0300

    (Я.Ф. Розділ 9, стор. 176). Практичні вправи: 1. Напишіть програму копіювання файлів під назвою FileCopy, об'єднавши для цього фрагменти коду з розділу про байтові потоки. Відкрийте два потоки (введення і виведення) і викличте методи read() і write() в одному і тому ж циклі. --- Програмній код для копіювання текстових файлів з використанням методів  FileInputStream та FileOutputStream ---

--- src/main/java/chapter_9_IO_streams/FileCopy.java
+++ src/main/java/chapter_9_IO_streams/FileCopy.java
@@ -1,20 +1,24 @@
 package chapter_9_IO_streams;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+
+
 
 public class FileCopy {
     public static void main(String[] args) throws IOException {
 
-        FileReader in = null;
-        FileWriter out = null;
+        // FileReader in = null;
+        FileInputStream in = null;
+        // FileWriter out = null;
+        FileOutputStream out = null;
+
+
 
         try {
-            in = new FileReader("/Users/ihorvoloshyn/IdeaProjects/StudyJF_JDK8_333" +
-                    "/src/main/java/chapter_9_IO_streams/scores.txt");
-            out = new FileWriter("/Users/ihorvoloshyn/IdeaProjects/StudyJF_JDK8_333" +
-                    "/src/main/java/chapter_9_IO_streams/scores_copy.txt");
+            in = new FileInputStream("/Users/ihorvoloshyn/IdeaProjects/StudyJF_JDK8_333" +
+                    "/src/main/java/chapter_9_IO_streams/cornflower.png");
+            out = new FileOutputStream("/Users/ihorvoloshyn/IdeaProjects/StudyJF_JDK8_333" +
+                    "/src/main/java/chapter_9_IO_streams/cornflower_copy.png");
 
             int c;
             while ((c = in.read()) != -1) {
